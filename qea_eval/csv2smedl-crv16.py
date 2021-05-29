43f937f9939fd776db085c5a5663c465979e1b4e
rahxephon89 <rahxephon89@163.com>
2021-05-29 18:46:27 -0400

    script

--- qea_eval/csv2smedl-crv16.py
+++ qea_eval/csv2smedl-crv16.py
@@ -25,6 +25,7 @@ with open('./'+input_file) as fp:
         #resource = int(arg1_str.split(' = ')[1])
         event = ''
         ev_name = lst[0]
+        quote = '\"'
         if ev_name == 'create_auction':
             channel = 'ch1'
             item = int(lst[1][4:])
@@ -67,7 +68,92 @@ with open('./'+input_file) as fp:
                 "params": [],
                 "aux": {{"line": {i}}}
                 }}"""     
+        elif ev_name == 'input':
+            channel = 'ch1'
+            sq = int(lst[1].strip()[13:],16)
+            event = f"""{{
+                "fmt_version": [2, 0],
+                "channel": "{channel}",
+                "event": "{ev_name}",
+                "params": [{sq}],
+                "aux": {{"line": {i}}}
+                }}"""
+        elif ev_name == 'sanitise':
+            channel = 'ch4'
+            sq = int(lst[1].strip()[13:],16)
+            event = f"""{{
+                "fmt_version": [2, 0],
+                "channel": "{channel}",
+                "event": "{ev_name}",
+                "params": [{sq}],
+                "aux": {{"line": {i}}}
+                }}"""
+        elif ev_name == 'derive':
+            channel = 'ch2'
+            sq1 = int(lst[1].strip()[13:],16)
+            sq2 = int(lst[2].strip()[13:],16)
+            event = f"""{{
+                "fmt_version": [2, 0],
+                "channel": "{channel}",
+                "event": "{ev_name}",
+                "params": [{sq1},{sq2}],
+                "aux": {{"line": {i}}}
+                }}"""
+        elif ev_name == 'use':
+            channel = 'ch3'
+            sq = int(lst[1].strip()[13:],16)
+            event = f"""{{
+                "fmt_version": [2, 0],
+                "channel": "{channel}",
+                "event": "{ev_name}",
+                "params": [{sq}],
+                "aux": {{"line": {i}}}
+                }}"""
+        elif ev_name == 'member':
+            channel = 'ch1'
+            v = lst[1].strip()
+            p = lst[2].strip()
+            event = f"""{{
+                "fmt_version": [2, 0],
+                "channel": "{channel}",
+                "event": "{ev_name}",
+                "params": [{quote}{v}{quote},{quote}{p}{quote}],
+                "aux": {{"line": {i}}}
+                }}""" 
+        elif ev_name == 'candidate':
+            channel = 'ch2'
+            v = lst[1].strip()
+            c = lst[2].strip()
+            event = f"""{{
+                "fmt_version": [2, 0],
+                "channel": "{channel}",
+                "event": "{ev_name}",
+                "params": [{quote}{v}{quote},{quote}{c}{quote}],
+                "aux": {{"line": {i}}}
+                }}""" 
+        elif ev_name == 'end':
+            channel = 'ch3'
+            event = f"""{{
+                "fmt_version": [2, 0],
+                "channel": "{channel}",
+                "event": "{ev_name}",
+                "params": [],
+                "aux": {{"line": {i}}}
+                }}""" 
+        elif ev_name == 'rank':
+            channel = 'ch7'
+            v = lst[1].strip()
+            c = lst[2].strip()
+            r = int(lst[3].strip())
+            event = f"""{{
+                "fmt_version": [2, 0],
+                "channel": "{channel}",
+                "event": "{ev_name}",
+                "params": [{quote}{v}{quote},{quote}{c}{quote}, {r}],
+                "aux": {{"line": {i}}}
+                }}"""        
         else:
+            print(ev_name)
             raise ValueError(f"Unknown event name {ev_name}")
         
         
